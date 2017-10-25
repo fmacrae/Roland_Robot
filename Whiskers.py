ee5312029b9a439f0cc6409dfdb5c7dbdac9630c
finlay macrae <finlay.macrae@gmail.com>
2017-10-25 22:47:38 +0000

    fixed bug in the Whiskers class

--- Whiskers.py
+++ Whiskers.py
@@ -7,19 +7,20 @@ GPIO.setup(21,GPIO.IN)
 
 
 class Whiskers(object):
-  prev_left = 0
-  prev_right = 0
+  def __init__(self):
+    self.prev_left = 0
+    self.prev_right = 0
   def checkBumpLeft(self):
     input_left = GPIO.input(20)
-    if ((not prev_left) and input_left):
+    if ((not self.prev_left) and input_left):
       print("ouch on left")
       speaker.say("oof bumped the left")
-    prev_left = input_left
+    self.prev_left = input_left
     return input_left
   def checkBumpRight(self):
     input_right = GPIO.input(21)
-    if ((not prev_right) and input_right):
+    if ((not self.prev_right) and input_right):
       print("ouch on right")
       speaker.say("ouch mind the right side")
-    prev_right = input_right
+    self.prev_right = input_right
     return input_right
