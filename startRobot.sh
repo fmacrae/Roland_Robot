dc4aae94b74250fc318db6777ffee15a76e22a07
finlay macrae <finlay.macrae@gmail.com>
2017-11-11 15:58:15 +0000

    swapped start order so log dir is known

--- startRobot.sh
+++ startRobot.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 sh setMaxVol.sh
-python recursive_image.py --image_file='/dev/shm/mjpeg/cam.jpg' --warmup_runs=1 --num_runs=40 &
 python Panning.py &
 python autonomous.py &
-
+python recursive_image.py --image_file='/dev/shm/mjpeg/cam.jpg' --warmup_runs=1 --num_runs=40 &
